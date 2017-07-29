bd8131d2cc687c090e4acdd796cb13e24aef3795
Yasuaki Takebe <takebe@cityfujisawa.ne.jp>
2017-07-29 23:15:38 +0900

    Added a LICENSE file and permission statement.

--- binmove.el
+++ binmove.el
@@ -1,5 +1,18 @@
 ;;; binmove.el --- Minor mode to move cursor up / down like binary search.
 
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;
 ;; Add setting to .emacs.el as follows:
 ;;
